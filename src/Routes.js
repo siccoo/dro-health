6a907197356c5c0ded59416cc2cbe95f6938ffb1
siccoo <obichilaka@gmail.com>
2021-03-02 11:49:43 +0100

    updates on Routes.js

---
+++ src/Routes.js
@@ -0,0 +1,13 @@
+import React from "react";
+import {
+    Route,
+    BrowserRouter as Router,
+    Switch,
+    Redirect,
+} from "react-router-dom";
+
+const Routes = (props) => (
+
+);
+
+export default Routes;
