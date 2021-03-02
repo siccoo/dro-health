b27342f6866b4941cc4ebf2b1776dcd559868e43
siccoo <obichilaka@gmail.com>
2021-03-02 12:14:34 +0100

    updates on index.js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import "bootstrap/dist/css/bootstrap.css";
 import reportWebVitals from './reportWebVitals';
+import Routes from "./Routes";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Routes />
   </React.StrictMode>,
   document.getElementById('root')
 );
