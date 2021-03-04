27edc2f75ac037dbdb316b087737173e208cc09c
siccoo <obichilaka@gmail.com>
2021-03-04 19:46:24 +0100

    updates on config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const config = {
-    baseUrl: "https://localhost:8000/patients/api", //no trialing slash here
+    baseUrl: "http://localhost:8080/patients/api", //no trialing slash here
   };
   
   export default config;
